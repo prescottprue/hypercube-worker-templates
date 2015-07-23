c68328736da45cf04ed8091fd0f32bd3d53ca228
Scott Prue <prescottprue@gmail.com>
2015-07-23 15:44:55 -0400

    Added test write to s3 bucket based on name provided in message.

--- app.js
+++ app.js
@@ -1,34 +1,43 @@
 var port = process.env.PORT || 3000,
     http = require('http'),
     fs = require('fs');
+var s3 = require('./lib/s3Util');
 
 var log = function(entry) {
     fs.appendFileSync('/tmp/sample-app.log', new Date().toISOString() + ' - ' + entry + '\n');
 };
 
 var server = http.createServer(function (req, res) {
-    if (req.method === 'POST') {
-        var body = '';
+  if (req.method === 'POST') {
+    var body = '';
 
-        req.on('data', function(chunk) {
-            body += chunk;
+    req.on('data', function(chunk) {
+        body += chunk;
+    });
+
+    req.on('end', function() {
+      if (req.url === '/') {
+        log('Received message: ' + body);
+        s3.uploadDir(body.split(":")[0], "./lib").then(function (file){
+          // res.writeHead(200, 'OK', {'Content-Type': 'application/json'});
+          log('Upload successful' + JSON.stringify(file));
+          res.json(file);
+        }, function(err){
+          log('Error uploading ' + JSON.stringify(err));
+          res.status(400);
         });
+      } else if (req.url = '/scheduled') {
+        log('Received task ' + req.headers['x-aws-sqsd-taskname'] + ' scheduled at ' + req.headers['x-aws-sqsd-scheduled-at']);
+      }
 
-        req.on('end', function() {
-            if (req.url === '/') {
-                log('Received message: ' + body);
-            } else if (req.url = '/scheduled') {
-                log('Received task ' + req.headers['x-aws-sqsd-taskname'] + ' scheduled at ' + req.headers['x-aws-sqsd-scheduled-at']);
-            }
-
-            res.writeHead(200, 'OK', {'Content-Type': 'text/plain'});
-            res.end();
-        });
-    } else {
-        res.writeHead(200);
-        res.write(html);
-        res.end();
-    }
+      res.writeHead(200, 'OK', {'Content-Type': 'text/plain'});
+      res.end();
+    });
+  } else {
+    res.writeHead(200);
+    res.write(html);
+    res.end();
+  }
 });
 
 // Listen on port 3000, IP defaults to 127.0.0.1
